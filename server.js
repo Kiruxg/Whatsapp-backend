51dcdd7957dca1c42e201ea4e8904596352afd40
Kirubel Girma <kirubelgirma0@gmail.com>
2020-09-26 11:30:18 -0700

    changed cors config

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ const pusher = new Pusher({
   cluster: "us3",
   encrypted: true
 })
-// const corsOptions = {
-//   origin: "https://whatsapp-mern-b640a.web.app",
-//   methods: "GET,POST,PUT,PATCH,DELETE"
-// }
+const corsOptions = {
+  origin: "*",
+  methods: "GET,POST,PUT,PATCH,DELETE",
+  allowedHeaders: "*"
+}
 // "http://localhost:3000"
 // pusher.trigger('my-channel', 'my-event', {
 //   'message': 'hello world'
@@ -24,10 +25,10 @@ const pusher = new Pusher({
 
 //middlewares
 app.use(express.json()) //parses incoming json objects
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"), res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE"), res.header("Access-Control-Allow-Headers", "*"), next()
-})
-// app.use(cors(corsOptions))
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"), res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE"), res.header("Access-Control-Allow-Headers", "*"), next()
+// })
+app.use(cors(corsOptions))
 //endpoints
 app.get("/", (req, res) => {
   res.status(200).send("Whatsappweb-backend")
