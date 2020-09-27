87601d02514376aa5142472f7a68e3ad162b4bfb
Kirubel Girma <kirubelgirma0@gmail.com>
2020-09-26 20:10:01 -0700

    changed header config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const Pusher = require("pusher")
 const Rooms = require("./dbRooms") //Message collection
 const app = express()
-// app.use(cors())
+app.use(cors({ credentials: true, origin: true }))
 const port = process.env.PORT || 9000 //heroku dev env or local env
 
 const pusher = new Pusher({
@@ -25,23 +25,23 @@ const pusher = new Pusher({
 // });
 
 //middlewares
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://whatsapp-mern-b640a.web.app/")
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "https://whatsapp-mern-b640a.web.app")
 
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+//   // Request methods you wish to allow
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
 
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+//   // Request headers you wish to allow
+//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true)
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true)
 
-  // Pass to next layer of middleware
-  next()
-})
+//   // Pass to next layer of middleware
+//   next()
+// })
 app.use((req, res, next) => {
   express.json()
   next()
